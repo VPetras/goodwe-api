c80eef676619e5cdf8d5607ea015ee7033f10757
Vojtěch Petrásek <vojtech.petrasek@comap-control.com>
2022-12-17 18:53:31 +0100

    init commit

---
+++ api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+###################################################
+# REST-API
+# Title: API to save measured data to influxdb
+# Author: Vojtěch Petrásek
+# Generated: 17/12/2022 18:50:21
+###################################################
+
+import sys
+
+def main():
+    print("Hello world!")
+
+while(True):
+    try:
+        main()
+    except Exception as e:
+        print(e)
+        sys.exit()
---
+++ app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+###################################################
+# Data extractor from goodwe
+# Title: Read data via RS485 from goodwe
+# Author: Vojtěch Petrásek
+# Generated: 17/12/2022 18:50:21
+###################################################
+
+import sys
+
+def main():
+    print("Hello world!")
+
+while(True):
+    try:
+        main()
+    except Exception as e:
+        print(e)
+        sys.exit()
